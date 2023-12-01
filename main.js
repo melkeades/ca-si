ed2e19a2345cc6b015632f9e5aa456d44a273e7f
melkeades <118681335+melkeades@users.noreply.github.com>
2023-12-01 17:37:46 +0300

    eds

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-import './style.styl'
+// import './style.styl'
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
-import MotionPathPlugin from 'gsap/MotionPathPlugin'
-import Lenis from '@studio-freight/lenis'
-import Splide from '@splidejs/splide'
-import '@splidejs/splide/css'
-import { AutoScroll } from '@splidejs/splide-extension-auto-scroll'
-import { Intersection } from '@splidejs/splide-extension-intersection'
 
 const vh = (percent) => window.innerHeight * (percent / 100)
-gsap.registerPlugin(ScrollTrigger, MotionPathPlugin)
+gsap.registerPlugin(ScrollTrigger)
 ScrollTrigger.create({
   animation: gsap.to('.navbar-sticky', { y: 0, opacity: 1 }, 0),
   trigger: 'body',
