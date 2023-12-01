3a6bb8c379913df3a9f260af3f06ab5bdd3e0f2f
melkeades <118681335+melkeades@users.noreply.github.com>
2023-12-01 16:56:28 +0300

    eds

--- main.js
+++ main.js
@@ -60,13 +60,12 @@ mq.add('(min-width: 991px)', () => {
   })
 })
 mq.add('(max-width: 767px)', () => {
-  // ScrollTrigger.create({
-  //   // animation: gsap.fromTo('.navbar-sticky', { yPercent: -100 }, { yPercent: 100 }),
-  //   animation: gsap.to('.navbar-sticky', { y: 0, opacity: 1 }, 0),
-  //   trigger: 'body',
-  //   start: vh(200) + ' top',
-  //   toggleActions: 'play none none reverse',
-  // })
+  ScrollTrigger.create({
+    animation: gsap.to('.navbar-sticky', { y: 0, opacity: 1 }, 0),
+    trigger: 'body',
+    start: 'top+=800px top',
+    toggleActions: 'play none none reverse',
+  })
 })
 devMode(0)
 function home() {
